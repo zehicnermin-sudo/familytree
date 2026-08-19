9c4d58c6ab405fb0c1fbf3aa948bf991def88f9e
zehicnermin-sudo <zehicnermin@gmail.com>
2026-08-19 21:02:42 +0200

    Update maiden names for Fatima (r. Bilalić) and Izeta (r. Avdić) under Šaćir lineage

--- porodicno_stablo_zehic.sql
+++ porodicno_stablo_zehic.sql
@@ -343,8 +343,8 @@ INSERT INTO "clanovi" VALUES('sp_sacir_osman','Alta','','Ž',3,'Grana Osman','',
 INSERT INTO "clanovi" VALUES('sacir_osman','Šaćir','Zehić','M',3,'Grana Osman','1914 – 1999','','osman_1','Osman','Alta','Supruga',0);
 INSERT INTO "clanovi" VALUES('mevla_sacir','Mevla','Zehić','Ž',4,'Grana Osman','','u. Mujagić (Centar)','sacir_osman','Šaćir','','',0);
 INSERT INTO "clanovi" VALUES('seida_sacir','Seida','Zehić','Ž',4,'Grana Osman','','u. Omerović (Šepak)','sacir_osman','Šaćir','','',0);
-INSERT INTO "clanovi" VALUES('sp_avdo_sacir','Izeta','','Ž',4,'Grana Osman','','Supruga','avdo_sacir','Avdo','Avdo','',1);
-INSERT INTO "clanovi" VALUES('avdo_sacir','Avdo','Zehić','M',4,'Grana Osman','','','sacir_osman','Šaćir','Izeta','Supruga',0);
+INSERT INTO "clanovi" VALUES('sp_avdo_sacir','Izeta','','Ž',4,'Grana Osman','','r. Avdić','avdo_sacir','Avdo','Avdo','',1);
+INSERT INTO "clanovi" VALUES('avdo_sacir','Avdo','Zehić','M',4,'Grana Osman','','','sacir_osman','Šaćir','Izeta','r. Avdić',0);
 INSERT INTO "clanovi" VALUES('sp_izudin_avdo_sacir','Maksida','','Ž',5,'Grana Osman','','r. Krezić','izudin_avdo_sacir','Izudin','Izudin','',1);
 INSERT INTO "clanovi" VALUES('izudin_avdo_sacir','Izudin','Zehić','M',5,'Grana Osman','','','avdo_sacir','Avdo','Maksida','r. Krezić',0);
 INSERT INTO "clanovi" VALUES('alma_izudin','Alma','Zehić','Ž',6,'Grana Osman','','u. Muslić (Brnjik)','izudin_avdo_sacir','Izudin','','',0);
@@ -360,8 +360,8 @@ INSERT INTO "clanovi" VALUES('sp_asmir_mevludin','Admira','','Ž',6,'Grana Osman
 INSERT INTO "clanovi" VALUES('asmir_mevludin','Asmir','Zehić','M',6,'Grana Osman','','','mevludin_avdo_sacir','Mevludin','Admira','r. Nukić',0);
 INSERT INTO "clanovi" VALUES('lejla_asmir_m','Lejla','Zehić','Ž',7,'Grana Osman','','','asmir_mevludin','Asmir','','',0);
 INSERT INTO "clanovi" VALUES('azijada_sacir','Azijada','Zehić','Ž',4,'Grana Osman','','u. Hamidović (Atmačići)','sacir_osman','Šaćir','','',0);
-INSERT INTO "clanovi" VALUES('sp_omer_sacir','Fatima','','Ž',4,'Grana Osman','','Supruga','omer_sacir','Omer','Omer','n. Poginuo 1995',1);
-INSERT INTO "clanovi" VALUES('omer_sacir','Omer','Zehić','M',4,'Grana Osman','1995','n. Poginuo 1995','sacir_osman','Šaćir','Fatima','Supruga',0);
+INSERT INTO "clanovi" VALUES('sp_omer_sacir','Fatima','','Ž',4,'Grana Osman','','r. Bilalić','omer_sacir','Omer','Omer','n. Poginuo 1995',1);
+INSERT INTO "clanovi" VALUES('omer_sacir','Omer','Zehić','M',4,'Grana Osman','1995','n. Poginuo 1995','sacir_osman','Šaćir','Fatima','r. Bilalić',0);
 INSERT INTO "clanovi" VALUES('zekira_omer_sacir','Zekira','Zehić','Ž',5,'Grana Osman','','u. Karasuljić (Čelić)','omer_sacir','Omer','','',0);
 INSERT INTO "clanovi" VALUES('aldin_zekira','Aldin','Zehić','M',6,'Grana Osman','','','zekira_omer_sacir','Zekira','','',0);
 INSERT INTO "clanovi" VALUES('naida_zekira','Naida','Zehić','Ž',6,'Grana Osman','','','zekira_omer_sacir','Zekira','','',0);
